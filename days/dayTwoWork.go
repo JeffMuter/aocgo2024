33e4560a2a7bcee2a622dc86646a57e58d46b51e
JeffMuter <muterjeffery@gmail.com>
2024-12-04 11:04:18 -0500

    day2 kicking my butt too hard.

--- days/dayTwoWork.go
+++ days/dayTwoWork.go
@@ -29,6 +29,7 @@ func DayTwoWork() (int, error) {
 		splitLineString := strings.Split(line, " ") // splits into slice of ["a", "b", "c", "d", "e"]string
 		var splitLineInt []int
 
+		// loop turns slice of strings into slice of ints
 		for i := range splitLineString {
 			convertedInt, err := strconv.Atoi(splitLineString[i])
 			if err != nil {
@@ -39,86 +40,64 @@ func DayTwoWork() (int, error) {
 			splitLineInt = append(splitLineInt, convertedInt)
 		}
 
-		if len(splitLineInt) == 0 {
-			return -1, fmt.Errorf("error converting strings to ints: %w\nline: %s/n", err, line)
+		// assert that the split slice of its isnt 0, and also the same length as expected.
+		if len(splitLineInt) != len(splitLineString) {
+			return -1, fmt.Errorf("error converting string slice to int slice. unequal lengths between them\n")
 		}
 
+		err = checkForSafe(splitLineInt)
+		if err != nil { // begin process to check if removing one number can be safe.
+			err = checkForNearlySafe(splitLineInt)
+			if err != nil {
+				fmt.Printf("line completely unsafe: %v\nline: %v\n\n", err, line)
+				continue
+			}
+			fmt.Printf("line is barely safe: %v\n\n", line)
+			safetyScore++
+			continue
+		}
+		fmt.Println("line completely safe: ", line)
+		fmt.Println()
+		safetyScore++
 	}
 
 	return safetyScore, nil
 }
 
 func checkForIncreasing(intSlice []int) error {
-	var phoenix bool
 	for i := 0; i < len(intSlice)-1; i++ {
 		if intSlice[i] < intSlice[i+1] {
 			// conditional check to make sure difference not greater than 3
 			if intSlice[i+1]-intSlice[i] > 3 {
-				fmt.Printf("revival status: index is: %d\n", i)
-				if !phoenix {
-					removeIndex(i, intSlice)
-					phoenix = true
-					i--
-					continue
-				}
-				return fmt.Errorf("difference between too great while increasing...")
+				return fmt.Errorf("difference between too great while increasing...\n")
 			}
 			continue
-		} else {
-			if !phoenix {
-				fmt.Printf("revival status: index is: %d\n", i)
-				removeIndex(i, intSlice)
-				phoenix = true
-				i--
-				continue
-			}
-			return fmt.Errorf("was not incrementing... not safe...")
 		}
+		return fmt.Errorf("was not incrementing... not safe...")
 	}
-
 	return nil
 }
 
 func checkForDecreasing(intSlice []int) error { // only called when index0 is confirmed less and i1
-	var phoenix bool
 	for i := 0; i < len(intSlice)-1; i++ {
 		if intSlice[i] > intSlice[i+1] {
 			// conditional check to make sure difference not greater than 3
 			if intSlice[i]-intSlice[i+1] > 3 {
-				if !phoenix {
-					fmt.Printf("revival status: index is: %d\n", i)
-					removeIndex(i, intSlice)
-					phoenix = true
-					i--
-					continue
-				}
-				return fmt.Errorf("difference between too great while decreasing...")
+				return fmt.Errorf("difference between too great while decreasing...\n")
 			}
 			continue
-		} else {
-			if !phoenix {
-				fmt.Printf("revival status: index is: %d\n", i)
-				removeIndex(i, intSlice)
-				phoenix = true
-				i--
-				continue
-			}
-			return fmt.Errorf("was not decrementing... not safe...")
 		}
+		return fmt.Errorf("was not decrementing, not safe...\n")
 	}
-
 	return nil
 }
 
 func removeIndex(index int, slice []int) []int {
-	fmt.Printf("removing index: [%d]\nslice\nline: %v\n", index, slice)
 	slice = append(slice[:index], slice[index+1:]...)
-	fmt.Printf("slice after removal: %v\n", slice)
 	return slice
 }
 
 func checkForSafe(slice []int) error {
-
 	if slice[0] < slice[1] {
 		err := checkForIncreasing(slice)
 		if err != nil {
@@ -129,6 +108,23 @@ func checkForSafe(slice []int) error {
 		if err != nil {
 			return fmt.Errorf("error:decreasing value proved unsafe... slice used: %v\n err value: %w\n", slice, err)
 		}
+	} else if slice[0] == slice[1] {
+		return fmt.Errorf("error checking for safe: index 0 and 1 are tied.")
 	}
 	return nil
+}
+
+func checkForNearlySafe(slice []int) error {
+	// loop through the slice, removing one element at a time, calling checkforsafe on each variation if any are safe, return nil, otherwise, if all variations arent safe, return err
+	for i := range slice {
+		var poppedSlice []int
+		poppedSlice = removeIndex(i, slice)
+		err := checkForSafe(poppedSlice)
+		if err != nil {
+			continue
+		}
+		fmt.Printf("altered line safe: %v... original: %v\n", poppedSlice, slice)
+		return nil // this can only trigger if slice is safe, so return nil
+	}
+	return fmt.Errorf("no version of this slice was safe...\n")
 }
